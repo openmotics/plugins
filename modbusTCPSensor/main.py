0b024a5badc645295a67143cab25bb87a5aad9cc
Pieter De Clerck <pieter.declerck@gmail.com>
2019-06-25 09:32:13 +0200

    Added possibility to set validation bits based on Modbus boolean values

--- modbusTCPSensor/main.py
+++ modbusTCPSensor/main.py
@@ -1,5 +1,5 @@
 """
-Get sensor values form modbus
+Get sensor values from modbus
 """
 
 import sys
@@ -15,7 +15,7 @@ class ModbusTCPSensor(OMPluginBase):
     """
 
     name = 'modbusTCPSensor'
-    version = '1.0.7'
+    version = '1.0.17'
     interfaces = [('config', '1.0')]
 
     config_description = [{'name': 'modbus_server_ip',
@@ -38,9 +38,17 @@ class ModbusTCPSensor(OMPluginBase):
                            'content': [{'name': 'sensor_id', 'type': 'int'},
                                        {'name': 'sensor_type', 'type': 'enum', 'choices': ['temperature',
                                                                                            'humidity',
-                                                                                           'brightness']},
+                                                                                           'brightness',
+                                                                                           'validation_bit']},
                                        {'name': 'modbus_address', 'type': 'int'},
-                                       {'name': 'modbus_register_length', 'type': 'int'}]}]
+                                       {'name': 'modbus_register_length', 'type': 'int'}]},
+                          {'name': 'bits',
+                           'type': 'section',
+                           'description': 'OM validation bit ID (e.g. 4), and a Modbus Coil Address',
+                           'repeat': True,
+                           'min': 0,
+                           'content': [{'name': 'validation_bit_id', 'type': 'int'},
+                                       {'name': 'modbus_coil_address', 'type': 'int'}]}]
 
     default_config = {'modbus_port': 502, 'sample_rate': 60}
 
@@ -68,9 +76,15 @@ class ModbusTCPSensor(OMPluginBase):
         self._debug = self._config.get('debug', 0) == 1
         self._sample_rate = self._config.get('sample_rate', ModbusTCPSensor.default_config['sample_rate'])
         self._sensors = []
+        # Load Sensors
         for sensor in self._config.get('sensors', []):
             if 0 <= sensor['sensor_id'] < 32:
                 self._sensors.append(sensor)
+        # Load Validation bits
+        self._validation_bits = []
+        for validation_bit in self._config.get('bits', []):
+            if 0 <= validation_bit['validation_bit_id'] < 256:
+                self._validation_bits.append(validation_bit)
         self._enabled = len(self._sensors) > 0
 
         try:
@@ -83,12 +97,6 @@ class ModbusTCPSensor(OMPluginBase):
 
         self.logger('ModbusTCPSensor is {0}'.format('enabled' if self._enabled else 'disabled'))
 
-    def clamp_sensor(self, value, sensor_type):
-        clamping = {'temperature': [-32, 95.5, 1],
-                    'humidity': [0, 100, 1],
-                    'brightness': [0, 100, 0]}
-        return round(max(clamping[sensor_type][0], min(value, clamping[sensor_type][1])), clamping[sensor_type][2])
-
     @background_task
     def run(self):
         while True:
@@ -96,33 +104,62 @@ class ModbusTCPSensor(OMPluginBase):
                 if not self._enabled or self._client is None:
                     time.sleep(5)
                     continue
-                om_sensors = {}
-                for sensor in self._sensors:
-                    registers = self._client.read_holding_registers(sensor['modbus_address'],
-                                                                    sensor['modbus_register_length'])
-                    if registers is None:
-                        continue
-                    sensor_value = struct.unpack('>f', struct.pack('BBBB',
-                                                                   registers[1] >> 8, registers[1] & 255,
-                                                                   registers[0] >> 8, registers[0] & 255))[0]
-                    if not om_sensors.get(sensor['sensor_id']):
-                        om_sensors[sensor['sensor_id']] = {'temperature': None, 'humidity': None, 'brightness': None}
-
-                    sensor_value = self.clamp_sensor(sensor_value, sensor['sensor_type'])
-
-                    om_sensors[sensor['sensor_id']][sensor['sensor_type']] = sensor_value
-                if self._debug == 1:
-                    self.logger('The sensors values are: {0}'.format(om_sensors))
-
-                for sensor_id, values in om_sensors.iteritems():
-                    result = json.loads(self.webinterface.set_virtual_sensor(sensor_id, **values))
-                    if result['success'] is False:
-                        self.logger('Error when updating virtual sensor {0}: {1}'.format(sensor_id, result['msg']))
+                # Process all configured sensors
+                self.process_sensors()
+                # Process all validation bits
+                self.process_validation_bits()
 
                 time.sleep(self._sample_rate)
             except Exception as ex:
                 self.logger('Could not process sensor values: {0}'.format(ex))
                 time.sleep(15)
+
+    def clamp_sensor(self, value, sensor_type):
+        clamping = {'temperature': [-32, 95.5, 1],
+                    'humidity': [0, 100, 1],
+                    'brightness': [0, 100, 0]}
+        return round(max(clamping[sensor_type][0], min(value, clamping[sensor_type][1])), clamping[sensor_type][2])
+
+    def process_sensors(self):
+        om_sensors = {}
+        for sensor in self._sensors:
+            registers = self._client.read_holding_registers(sensor['modbus_address'],
+                                                            sensor['modbus_register_length'])
+            if registers is None:
+                continue
+            sensor_value = struct.unpack('>f', struct.pack('BBBB',
+                                                           registers[1] >> 8, registers[1] & 255,
+                                                           registers[0] >> 8, registers[0] & 255))[0]
+            if not om_sensors.get(sensor['sensor_id']):
+                om_sensors[sensor['sensor_id']] = {'temperature': None, 'humidity': None, 'brightness': None}
+
+            sensor_value = self.clamp_sensor(sensor_value, sensor['sensor_type'])
+
+            om_sensors[sensor['sensor_id']][sensor['sensor_type']] = sensor_value
+        if self._debug == 1:
+            self.logger('The sensors values are: {0}'.format(om_sensors))
+
+        for sensor_id, values in om_sensors.iteritems():
+            result = json.loads(self.webinterface.set_virtual_sensor(sensor_id, **values))
+            if result['success'] is False:
+                self.logger('Error when updating virtual sensor {0}: {1}'.format(sensor_id, result['msg']))
+
+    def process_validation_bits(self):
+        for validation_bit in self._validation_bits:
+            bit = self._client.read_coils(validation_bit['modbus_coil_address'], 1)
+
+            if bit is None or len(bit) != 1:
+                if self._debug == 1:
+                    self.logger('Failed to read bit {0}, bit is {1}'.format(validation_bit['validation_bit_id'], bit))
+                continue
+            result = json.loads(self.webinterface.do_basic_action(None,
+                                                                  237 if bit[0] else 238,
+                                                                  validation_bit['validation_bit_id']))
+            if result['success'] is False:
+                self.logger('Failed to set bit {0} to {1}'.format(validation_bit['validation_bit_id'], 1 if bit[0] else 0))
+            else:
+                if self._debug == 1:
+                    self.logger('Successfully set bit {0} to {1}'.format(validation_bit['validation_bit_id'], 1 if bit[0] else 0))
 
     @om_expose
     def get_config_description(self):
