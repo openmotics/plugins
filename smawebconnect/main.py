03d65dafecd3639d44723a3e716db73dc18fc75c
Sander Van Schoote <vanschoote.sander@gmail.com>
2020-02-14 09:39:59 +0100

    Bumped sma version to 0.21

--- smawebconnect/main.py
+++ smawebconnect/main.py
@@ -26,7 +26,7 @@ class SMAWebConnect(OMPluginBase):
     """
 
     name = 'SMAWebConnect'
-    version = '0.0.20'
+    version = '0.0.21'
     interfaces = [('config', '1.0'), ('metrics', '1.0')]
 
     config_description = [{'name': 'sample_rate',
