a5812f40c17a8cb4dd4c92b39e8b2619d7bc6a64
Sander Van Schoote <vanschoote.sander@gmail.com>
2020-08-04 14:49:44 +0200

    Clean pyc files upon packaging

--- package.sh
+++ package.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+pyclean () {
+        find . -type f -name "*.py[co]" -delete
+        find . -type d -name "__pycache__" -delete
+}
 cd $1
+pyclean
 version=`cat main.py | grep version | cut -d "\"" -f 2 | cut -d "'" -f 2 | head -n 1`
 tgzname=$1_$version
 tar -czf $tgzname.tgz *
