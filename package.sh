47d21fcfa28dcb6a3fb8530459731da4763fd869
Yannick Clybouw <yannick.clybouw@openmotics.com>
2020-08-12 10:17:41 +0200

    Optional version for package.sh and make compatible with Linux

--- package.sh
+++ package.sh
@@ -2,33 +2,52 @@
 
 set -eu
 
-if [[ $# -ne 2 ]]
+if [[ $# -eq 1 ]]
 then
+    plugin=$1
+    version=''
+elif [[ $# -eq 2 ]]
+then
+    plugin=$1
+    version=$2
+else
     echo """Invalid number of arguments
-    Usage: ./package.sh <plugin> <version>
+    Usage: ./package.sh <plugin> [<version>]
     <plugin> is the name of the plugin, located in the directory with the same name
-    <version> is used to update main.py of the plugin"""
+    <version> (optional) is used to update main.py of the plugin"""
     exit 1
 fi
 
-plugin=$1
-version=$2
-tar_file=${plugin}_${version}.tgz
-md5_file=${plugin}_${version}.md5
-
 if [ $(uname -s) == 'Darwin' ]
 then
+  sedcmd="sed -i ''"
+  tarcmd="tar -L"
   md5cmd='md5'
 else
+  sedcmd='sed -i'
+  tarcmd="tar -h"
   md5cmd='md5sum'
 fi
 
 cd ${plugin}
+
 find . -type f -name "*.py[co]" -delete
 find . -type d -name "__pycache__" -delete
-sed -i '' -E "s/version = '([0-9]+\.[0-9]+\.[0-9]+)'/version = '$version'/" main.py
-tar -cLzf ${tar_file} *
+
+if [[ -z "$version" ]]
+then
+    version=`cat main.py | grep version | cut -d "\"" -f 2 | cut -d "'" -f 2 | head -n 1`
+fi
+
+tar_file=${plugin}_${version}.tgz
+md5_file=${plugin}_${version}.md5
+
+${sedcmd} -E "s/version *= *('|\")[0-9]+\.[0-9]+\.[0-9]+('|\")/version = '$version'/" main.py
+
+${tarcmd} -czf ${tar_file} *
 mv ${tar_file} ..
+
 cd ..
+
 ${md5cmd} ${tar_file} > ${md5_file}
 cat ${md5_file}
