c8bafbd1629092f2727709ad633ef399c4b95622
Sander Van Schoote <vanschoote.sander@gmail.com>
2023-05-30 16:03:27 +0200

    Made dummy implementations for all assets

--- dummy/hotwater.py
+++ dummy/hotwater.py
@@ -6,15 +6,14 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class MockHotWater:
-
-    def __init__(self, unit, report_status, update_interval=5):
-        self.unit = unit
+class HotWaterDummy:
+    def __init__(self, hot_water_dto, report_status, update_interval=5):
+        self.hot_water_dto = hot_water_dto
 
         self.steering_power = 0
-        self.current_temperature = self.unit.min_temp
-        self.setpoint = self.unit.max_temp
-        self.state = 'on'
+        self.current_temperature = self.hot_water_dto.min_temp
+        self.setpoint = self.hot_water_dto.max_temp
+        self.state = "on"
 
         self.report_status = report_status
 
@@ -34,12 +33,16 @@ class MockHotWater:
 
     def simulation(self):
         while self._running:
-            changed = False
-            with self.lock:
-                changed |= self.update_steering_power()
-                changed |= self.update_current_temperature()
-                if changed:
-                    self.report_status(self.unit.id, self.steering_power, self.current_temperature)
+            changed = self.update_steering_power()
+            changed |= self.update_current_temperature()
+            if changed:
+                self.report_status(
+                    self.hot_water_dto,
+                    self.steering_power,
+                    self.current_temperature,
+                    self.setpoint,
+                    self.state,
+                )
             time.sleep(self.update_interval)
 
     def set_state(self, state):
@@ -51,7 +54,7 @@ class MockHotWater:
             self.setpoint = setpoint
 
     def update_steering_power(self):
-        if self.state == 'on':
+        if self.state == "on":
             if self.current_temperature >= self.setpoint:
                 new_steering_power = 0
             elif self.current_temperature >= self.setpoint - 10:
@@ -63,13 +66,20 @@ class MockHotWater:
         else:
             new_steering_power = 0
         changed = self.steering_power != new_steering_power
-        self.steering_power = new_steering_power
+        with self.lock:
+            self.steering_power = new_steering_power
         return changed
 
     def update_current_temperature(self):
         previous_current_temperature = self.current_temperature
-        delta_t = random.randint(0, 1) if (self.state == 'on' and self.steering_power > 0) else random.randint(-1, 0)
-        self.current_temperature = max(self.unit.min_temp, min(self.current_temperature + delta_t, self.unit.max_temp))
+        delta_t = (
+            random.randint(0, 1)
+            if (self.state == "on" and self.steering_power > 0)
+            else random.randint(-1, 0)
+        )
+        with self.lock:
+            self.current_temperature = max(
+                self.hot_water_dto.min_temp,
+                min(self.current_temperature + delta_t, self.hot_water_dto.max_temp),
+            )
         return previous_current_temperature != self.current_temperature
-
-
---
+++ dummy/ventilation.py
@@ -0,0 +1,84 @@
+import random
+import time
+from threading import Thread, Lock
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class VentilationDummy:
+    def __init__(self, ventilation_dto, report_status, update_interval=5):
+        self.ventilation_dto = ventilation_dto
+
+        self.mode = "auto"
+        self.level = self.ventilation_dto.min_level
+        self.remaining_time = None
+        self._expiry = None
+
+        self.report_status = report_status
+
+        self.thread = Thread(target=self.simulation)
+        self.update_interval = update_interval
+        self._running = False
+        self.lock = Lock()
+
+    def start(self):
+        logger.info("starting VentilationDummy")
+        self._running = True
+        self.thread.start()
+
+    def stop(self):
+        logger.info("stopping VentilationDummy")
+        self._running = False
+
+    def simulation(self):
+        while self._running:
+            changed = self.update()
+            if changed:
+                self.report_status(
+                    self.ventilation_dto, self.mode, self.level, self.remaining_time
+                )
+            time.sleep(self.update_interval)
+
+    def set_auto(self):
+        with self.lock:
+            self.mode = "auto"
+
+    def set_manual(self, level, timer):
+        with self.lock:
+            self.mode = "manual"
+            self.level = level
+            self._expiry = time.time() + timer
+
+    def update(self):
+        if self.mode == "auto":
+            new_remaining_time = None
+            new_expiry = None
+            new_mode = self.mode
+            new_level = random.randint(
+                self.ventilation_dto.min_level, self.ventilation_dto.max_level
+            )
+        else:
+            now = time.time()
+            new_remaining_time = (
+                self._expiry - now if self._expiry and now <= self._expiry else None
+            )
+            if new_remaining_time >= 0:
+                new_expiry = self._expiry
+                new_mode = self.mode
+            else:
+                new_expiry = None
+                new_mode = "auto"
+            new_level = self.level
+        changed = (
+            self.remaining_time != new_remaining_time
+            or self._expiry != new_expiry
+            or self.mode != new_mode
+            or self.level != new_level
+        )
+        with self.lock:
+            self.remaining_time = new_remaining_time
+            self._expiry = new_expiry
+            self.mode = new_mode
+            self.level = new_level
+        return changed
